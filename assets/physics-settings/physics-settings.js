fc198af1e6fc153262aec91a6c685a8b55438e5d
2youyouo2 <501251991@qq.com>
2017-04-14 14:16:13 +0800

    enable physics-settings

--- assets/physics-settings/physics-settings.js
+++ assets/physics-settings/physics-settings.js
@@ -2,7 +2,7 @@ let physicsManager = cc.director.getPhysicsManager();
 physicsManager.enabled = true;
 
 physicsManager.debugDrawFlags = 
-    0;
+    // 0;
     // cc.PhysicsManager.DrawBits.e_aabbBit |
     // cc.PhysicsManager.DrawBits.e_pairBit |
     // cc.PhysicsManager.DrawBits.e_centerOfMassBit |
