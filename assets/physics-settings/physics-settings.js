ef0a7033a65a1fb27fa1e08d5acc5c6aaf76f4b1
yinkaile <501251991@qq.com>
2018-03-02 16:16:44 +0800

    remove unsupport draw bits

--- assets/physics-settings/physics-settings.js
+++ assets/physics-settings/physics-settings.js
@@ -4,8 +4,6 @@ physicsManager.enabled = true;
 physicsManager.debugDrawFlags = 
     // 0;
     // cc.PhysicsManager.DrawBits.e_aabbBit |
-    // cc.PhysicsManager.DrawBits.e_pairBit |
-    // cc.PhysicsManager.DrawBits.e_centerOfMassBit |
     cc.PhysicsManager.DrawBits.e_jointBit |
     cc.PhysicsManager.DrawBits.e_shapeBit
     ;
