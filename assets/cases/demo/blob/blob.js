05d9bc3c34023e66e69a081de56556c76bf455e0
yinkaile <501251991@qq.com>
2018-05-09 10:32:45 +0800

    upgrade to 2.0

--- assets/cases/demo/blob/blob.js
+++ assets/cases/demo/blob/blob.js
@@ -15,8 +15,8 @@ cc.Class({
         this.ctx = this.getComponent(cc.Graphics);
 
         this.ctx.lineWidth = 6;
-        this.ctx.strokeColor = cc.hexToColor('#495069');
-        this.ctx.fillColor = cc.hexToColor('#ffde59');
+        this.ctx.strokeColor = cc.color().fromHEX('#495069');
+        this.ctx.fillColor = cc.color().fromHEX('#ffde59');
 
         let x = this.node.x;
         let y = this.node.y;
--- assets/cases/utils/cases-settings.js
+++ assets/cases/utils/cases-settings.js
@@ -1,3 +1,3 @@
 if (!CC_EDITOR) {
-    cc.director.setClearColor( cc.hexToColor('#2f69d2') );
+    cc.director.setClearColor( cc.color().fromHEX('#2f69d2') );
 }
