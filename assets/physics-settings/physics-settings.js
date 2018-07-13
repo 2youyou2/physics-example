787856744f4301fd660f0843a97193b5b62ee7e0
yinkaile <501251991@qq.com>
2018-07-13 15:33:30 +0800

    should modify physics settings after engine inited

--- assets/physics-settings/physics-settings.js
+++ assets/physics-settings/physics-settings.js
@@ -1,9 +1,12 @@
-let physicsManager = cc.director.getPhysicsManager();
-physicsManager.enabled = true;
+cc.game.on(cc.game.EVENT_ENGINE_INITED, () => {
+    let physicsManager = cc.director.getPhysicsManager();
+    physicsManager.enabled = true;
+    
+    physicsManager.debugDrawFlags = 
+        // 0;
+        // cc.PhysicsManager.DrawBits.e_aabbBit |
+        cc.PhysicsManager.DrawBits.e_jointBit |
+        cc.PhysicsManager.DrawBits.e_shapeBit
+        ;
+});
 
-physicsManager.debugDrawFlags = 
-    // 0;
-    // cc.PhysicsManager.DrawBits.e_aabbBit |
-    cc.PhysicsManager.DrawBits.e_jointBit |
-    cc.PhysicsManager.DrawBits.e_shapeBit
-    ;
